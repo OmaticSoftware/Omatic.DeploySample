4ee2cacf44026140f1763ae24f24927a42d031b1
Matt Hall <matt.hall@omaticsoftware.com>
2016-03-23 08:04:46 -0400

    Standardized PS whitespace

--- Omatic.Deploy/CustomizationInstall.ps1
+++ Omatic.Deploy/CustomizationInstall.ps1
@@ -15,8 +15,8 @@ param
 	[parameter()]
 	[switch]$NoInstall,
 
-    [parameter()]
-    [switch]$NonInteractive
+	[parameter()]
+	[switch]$NonInteractive
 )
 
 # Powershell v2 doesn't includ PSScriptRoot
@@ -27,9 +27,9 @@ if ($PSVersionTable.PSVersion.Major -eq 2) {
 # Logging setup
 $LogFormat = "{0:yyyyMMdd HHmmss} {1} {2}"
 if ($LogFile.Length -eq 0) {
-    $LogFile = Join-Path $PSScriptRoot -ChildPath "InstallLog.txt"
+	$LogFile = Join-Path $PSScriptRoot -ChildPath "InstallLog.txt"
 } else {
-    $LogFile = [System.IO.Path]::GetFullPath($LogFile)
+	$LogFile = [System.IO.Path]::GetFullPath($LogFile)
 }
 
 # Credit for the following: https://rkeithhill.wordpress.com/2013/04/05/powershell-script-that-relaunches-as-admin/
@@ -38,92 +38,92 @@ If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 	Write-Host "Not running as admin, elevating permissions."
 	Write-Host "LogFile: $LogFile."
 	[string[]]$argList = @("& '$($MyInvocation.MyCommand.Path)'")
-    $argList += "-LogFile '$LogFile'"
-    $argList += "-InstanceName '$InstanceName'"
+	$argList += "-LogFile '$LogFile'"
+	$argList += "-InstanceName '$InstanceName'"
 	if ($InstallWithRestore) { $argList += "-InstallWithRestore" }
-    if ($NoInstall) { $argList += "-NoInstall" }
-    if ($NonInteractive) { $argList += "-NonInteractive" }
-    #"No Administrative rights, it will display a popup window asking user for Admin rights"
-    Start-Process "$psHome\powershell.exe" -Verb runAs -WorkingDirectory $pwd -ArgumentList $argList
-    break
+	if ($NoInstall) { $argList += "-NoInstall" }
+	if ($NonInteractive) { $argList += "-NonInteractive" }
+	#"No Administrative rights, it will display a popup window asking user for Admin rights"
+	Start-Process "$psHome\powershell.exe" -Verb runAs -WorkingDirectory $pwd -ArgumentList $argList
+	break
 }
 
 # Logging functions
 function Log-Error {
-    param ([string]$msg)
-    Write-Error $msg
-    Add-Content $LogFile -Value ($LogFormat -f (Get-Date), "ERROR", $msg)
+	param ([string]$msg)
+	Write-Error $msg
+	Add-Content $LogFile -Value ($LogFormat -f (Get-Date), "ERROR", $msg)
 }
 
 function Log-Warning {
-    param ([string]$msg)
-    Write-Warning $msg
-    Add-Content $LogFile -Value ($LogFormat -f (Get-Date), "WARN", $msg)
+	param ([string]$msg)
+	Write-Warning $msg
+	Add-Content $LogFile -Value ($LogFormat -f (Get-Date), "WARN", $msg)
 }
 
 function Log-Info {
-    param ([string]$msg)
-    Add-Content $LogFile -Value ($LogFormat -f (Get-Date), "INFO", $msg)
+	param ([string]$msg)
+	Add-Content $LogFile -Value ($LogFormat -f (Get-Date), "INFO", $msg)
 }
 
 
 function Get-CrmInstallPath {
-    param([string]$crmInstanceName)
+	param([string]$crmInstanceName)
 
-    # Assumes 32-bit BB installer on 64-bit machine
-    $INSTALLERREGKEY = "HKLM:\Software\Wow6432Node\Blackbaud\AppFx"
+	# Assumes 32-bit BB installer on 64-bit machine
+	$INSTALLERREGKEY = "HKLM:\Software\Wow6432Node\Blackbaud\AppFx"
 
-    $installPath = ''
+	$installPath = ''
 
-    $bbInstalls = Get-ChildItem -Path $INSTALLERREGKEY
-    if ($bbInstalls.Length -eq 0) {
-	    $errMsg = "ERROR: Did not find any BB installs at reg key '$INSTALLERREGKEY'"
-	    Throw $errMsg
-    } else {
-        Log-Info "Found $($bbInstalls.Length) Blackbaud\AppFx install(s)"
+	$bbInstalls = Get-ChildItem -Path $INSTALLERREGKEY
+	if ($bbInstalls.Length -eq 0) {
+		$errMsg = "ERROR: Did not find any BB installs at reg key '$INSTALLERREGKEY'"
+		Throw $errMsg
+	} else {
+		Log-Info "Found $($bbInstalls.Length) Blackbaud\AppFx install(s)"
 		foreach ($bbInstall in $bbInstalls) 
 		{
 			$regEntry = Get-Item $bbInstall.PSPath
-            $installName = $regEntry.GetValue("InstallName")
-            Log-Info "Examining InstallName: '$installName'"
+			$installName = $regEntry.GetValue("InstallName")
+			Log-Info "Examining InstallName: '$installName'"
 			if (($installName.Length -eq 0 -and $crmInstanceName.Length -eq 0) -or ($installName -ieq $crmInstanceName))
-            { 
+			{ 
 				$installPath = $regEntry.GetValue("InstallPath")
 				Log-Info "Found installPath: '$installPath' for instance: '$InstanceName'"
 			}
 		}
-    }
+	}
 
-    return $installPath
+	return $installPath
 }
 
 
 function Unpack-Files {
-    param(
-        [parameter()]
-        [string]$zipFile,
+	param(
+		[parameter()]
+		[string]$zipFile,
 
-        [parameter()]
-        [string]$unpackFolder
-    )
+		[parameter()]
+		[string]$unpackFolder
+	)
 
-    Log-Info "Unpacking '$zipFile' to '$unpackFolder'"
+	Log-Info "Unpacking '$zipFile' to '$unpackFolder'"
 
 	[IO.Compression.ZipFile]::ExtractToDirectory($zipFile, $unpackFolder)
 }
 
 function Check-DirectoryIsEmpty {
-    param ([string]$dir) 
+	param ([string]$dir) 
 
-    $isEmpty = $True
-    foreach ($item in (Get-ChildItem $dir -Recurse -Force)) {
-        if (-not($item -is [System.IO.DirectoryInfo])) {
-            $isEmpty = $False
-            break
-        }
-    }
+	$isEmpty = $True
+	foreach ($item in (Get-ChildItem $dir -Recurse -Force)) {
+		if (-not($item -is [System.IO.DirectoryInfo])) {
+			$isEmpty = $False
+			break
+		}
+	}
 
-    return $isEmpty
+	return $isEmpty
 }
 
 function Copy-CustomizationFiles {
@@ -201,19 +201,19 @@ function Copy-CustomizationFiles {
 	$folderMap.Add($src, $customInstallPath)
 
 	# Do the copying, write each copied item to the manifest file
-    if (-not(Test-Path $manifestFile)) { 
-        New-Item $manifestFile -ItemType File > $null
-    }
-    $manifestDirectory = (Get-Item $manifestFile).Directory
+	if (-not(Test-Path $manifestFile)) { 
+		New-Item $manifestFile -ItemType File > $null
+	}
+	$manifestDirectory = (Get-Item $manifestFile).Directory
 	foreach ($map in $folderMap.GetEnumerator()) {
-        $source = $map.Key
-        $target = $map.Value
+		$source = $map.Key
+		$target = $map.Value
 
-        $robocopyLogFile = [System.IO.Path]::Combine($manifestDirectory, 'robocopylog.txt')
+		$robocopyLogFile = [System.IO.Path]::Combine($manifestDirectory, 'robocopylog.txt')
 
 		Log-Info "Copying from '$source' to '$target', writing copied files to '$robocopyLogFile'" 
-        
-        robocopy $source $target /s /is /np /njh /njs /ns /nc /fp /xx /log:$robocopyLogFile *>> $LogFle
+		
+		robocopy $source $target /s /is /np /njh /njs /ns /nc /fp /xx /log:$robocopyLogFile *>> $LogFle
 
 		$counter = 0
 		foreach ($line in (Get-Content $robocopyLogFile)) {
@@ -246,7 +246,7 @@ function Uninstall-CustomizationFiles {
 	} 
 	Log-Info "Found uninstall script at '$uninstallScript', performing uninstall"
 
-    $cmd = "& '$uninstallScript' -LogFile '$LogFile' -NonInteractive"
+	$cmd = "& '$uninstallScript' -LogFile '$LogFile' -NonInteractive"
 
 	Invoke-Expression $cmd
 }
@@ -296,37 +296,37 @@ Import-Module -Name ([System.IO.Path]::Combine($PSScriptRoot, "Invoke-MsBuild.ps
 $tempPath = [System.IO.Path]::Combine(([System.IO.Path]::GetTempPath()), 'BBCRMCustomInstall')
 if (Test-Path $tempPath) {
 	try {
-        Remove-Item $tempPath -Recurse -Force
-    } catch {
-        Log-Warning "Could not remove '$($tempPath)': $($_.Exception)"
-    }
+		Remove-Item $tempPath -Recurse -Force
+	} catch {
+		Log-Warning "Could not remove '$($tempPath)': $($_.Exception)"
+	}
 }
 
 try
 {
 	$installPath = Get-CrmInstallPath $InstanceName
-    if ($installPath.Length -eq 0) {
-        throw "Failed to find install path for CRM instance '$InstanceName'"
-    }
+	if ($installPath.Length -eq 0) {
+		throw "Failed to find install path for CRM instance '$InstanceName'"
+	}
 
-    $zipFile = [System.IO.Path]::Combine($PSScriptRoot, 'DeployFiles.zip')
-    if (-not(Test-Path $zipFile)) {
-        throw "Failed to find zip file '$zipFile'"
-    }
+	$zipFile = [System.IO.Path]::Combine($PSScriptRoot, 'DeployFiles.zip')
+	if (-not(Test-Path $zipFile)) {
+		throw "Failed to find zip file '$zipFile'"
+	}
 
-    Unpack-Files $zipFile $tempPath
+	Unpack-Files $zipFile $tempPath
 
 	$manifestFile = [System.IO.Path]::Combine($tempPath, 'BBCRMCustomInstallManifest.txt')
 
-    $customInstallPath = [System.IO.Path]::Combine($installPath, 'bbAppFx', 'MSBuild', 'Custom')
+	$customInstallPath = [System.IO.Path]::Combine($installPath, 'bbAppFx', 'MSBuild', 'Custom')
 	
-    Log-Info "Using custom install path: '$customInstallPath'"
+	Log-Info "Using custom install path: '$customInstallPath'"
 
-    if (-not(Test-Path $customInstallPath)) {
-        New-Item $customInstallPath -Type Directory > $null
-    } else {
-        Uninstall-CustomizationFiles $customInstallPath $manifestFile
-    }
+	if (-not(Test-Path $customInstallPath)) {
+		New-Item $customInstallPath -Type Directory > $null
+	} else {
+		Uninstall-CustomizationFiles $customInstallPath $manifestFile
+	}
 
 	Copy-CustomizationFiles $tempPath $installPath $customInstallPath $manifestFile
 
@@ -339,18 +339,18 @@ try
 } 
 catch 
 {
-    Log-Error $_.Exception
-    if (-not($NonInteractive)) {
-        Read-Host "Error encountered, see '$LogFile' for detail.  Press any key to continue..." > $null
-    } else {
-        throw
-    }
+	Log-Error $_.Exception
+	if (-not($NonInteractive)) {
+		Read-Host "Error encountered, see '$LogFile' for detail.  Press any key to continue..." > $null
+	} else {
+		throw
+	}
 }
 finally
 {
-    if (Test-Path $tempPath) {
-        Log-Info "Cleaning up '$tempPath'"
-        Remove-Item $tempPath -Recurse -Force
-    }
+	if (Test-Path $tempPath) {
+		Log-Info "Cleaning up '$tempPath'"
+		Remove-Item $tempPath -Recurse -Force
+	}
 	Log-Info "********** END INSTALLATION **********"
 }
--- Omatic.Deploy/CustomizationUninstall.ps1
+++ Omatic.Deploy/CustomizationUninstall.ps1
@@ -5,20 +5,20 @@ param
 (
 	[parameter()]
 	[string]$LogFile="",
-    
-    [parameter()]
-    [string]$ManifestFile="",
+	
+	[parameter()]
+	[string]$ManifestFile="",
 
-    [parameter()]
-    [switch]$NonInteractive
+	[parameter()]
+	[switch]$NonInteractive
 )
 
 # Logging setup
 $LogFormat = "{0:yyyyMMdd HHmmss} {1} {2}"
 if ($LogFile.Length -eq 0) {
-    $LogFile = Join-Path $PSScriptRoot -ChildPath "UninstallLog.txt"
+	$LogFile = Join-Path $PSScriptRoot -ChildPath "UninstallLog.txt"
 } else {
-    $LogFile = [System.IO.Path]::GetFullPath($LogFile)
+	$LogFile = [System.IO.Path]::GetFullPath($LogFile)
 }
 
 # Credit for the following: https://rkeithhill.wordpress.com/2013/04/05/powershell-script-that-relaunches-as-admin/
@@ -27,47 +27,47 @@ If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 	Write-Host "Not running as admin, elevating permissions."
 	Write-Host "LogFile: $LogFile."
 	[string[]]$argList = @("& '$($MyInvocation.MyCommand.Path)'")
-    foreach ($argItem in $MyInvocation.BoundParameters.GetEnumerator()) {
-        $argList += "-$($argItem.Key)"
-        $argList += "'$($argItem.Value)'"
-    }
-    $argList += $MyInvocation.UnboundArguments
-    #"No Administrative rights, it will display a popup window asking user for Admin rights"
-    write-Host $argList
-    Start-Process "$psHome\powershell.exe" -Verb runAs -WorkingDirectory $pwd -ArgumentList $argList
-    break
+	foreach ($argItem in $MyInvocation.BoundParameters.GetEnumerator()) {
+		$argList += "-$($argItem.Key)"
+		$argList += "'$($argItem.Value)'"
+	}
+	$argList += $MyInvocation.UnboundArguments
+	#"No Administrative rights, it will display a popup window asking user for Admin rights"
+	write-Host $argList
+	Start-Process "$psHome\powershell.exe" -Verb runAs -WorkingDirectory $pwd -ArgumentList $argList
+	break
 }
 
 # Logging functions
 function Log-Error {
-    param ([string]$msg)
-    Write-Error $msg
-    Add-Content $LogFile -Value ($LogFormat -f (Get-Date), "ERROR", $msg)
+	param ([string]$msg)
+	Write-Error $msg
+	Add-Content $LogFile -Value ($LogFormat -f (Get-Date), "ERROR", $msg)
 }
 
 function Log-Warning {
-    param ([string]$msg)
-    Write-Warning $msg
-    Add-Content $LogFile -Value ($LogFormat -f (Get-Date), "WARN", $msg)
+	param ([string]$msg)
+	Write-Warning $msg
+	Add-Content $LogFile -Value ($LogFormat -f (Get-Date), "WARN", $msg)
 }
 
 function Log-Info {
-    param ([string]$msg)
-    Add-Content $LogFile -Value ($LogFormat -f (Get-Date), "INFO", $msg)
+	param ([string]$msg)
+	Add-Content $LogFile -Value ($LogFormat -f (Get-Date), "INFO", $msg)
 }
 
 function Check-DirectoryIsEmpty {
-    param ([string]$dir) 
+	param ([string]$dir) 
 
-    $isEmpty = $True
-    foreach ($item in (Get-ChildItem $dir -Recurse -Force)) {
-        if (-not($item -is [System.IO.DirectoryInfo])) {
-            $isEmpty = $False
-            break
-        }
-    }
+	$isEmpty = $True
+	foreach ($item in (Get-ChildItem $dir -Recurse -Force)) {
+		if (-not($item -is [System.IO.DirectoryInfo])) {
+			$isEmpty = $False
+			break
+		}
+	}
 
-    return $isEmpty
+	return $isEmpty
 }
 
 
@@ -76,7 +76,7 @@ function Check-DirectoryIsEmpty {
 Log-Info "Starting customization uninstall."
 
 if ($ManifestFile.Length -eq 0) {
-    $ManifestFile = [System.IO.Path]::Combine($PSScriptRoot, 'BBCRMCustomInstallManifest.txt')
+	$ManifestFile = [System.IO.Path]::Combine($PSScriptRoot, 'BBCRMCustomInstallManifest.txt')
 }
 
 $warnings = @()
@@ -84,62 +84,62 @@ $warnings = @()
 try
 {
 	if (-not(Test-Path $ManifestFile)) {
-        throw "Could not find manifest file at '$ManifestFile'"
-    }
+		throw "Could not find manifest file at '$ManifestFile'"
+	}
 
-    # Keep track of a few extra things to clean up that might not be in the manifest
-    $cleanupArr = @(
-        [System.IO.Path]::Combine($PSScriptRoot, '..\..\vroot\bin\custom'),
-        [System.IO.Path]::Combine($PSScriptRoot, '..\..\vroot\browser\htmlforms\custom'),
-        [System.IO.Path]::Combine($PSScriptRoot, 'RevisionDlls'),
-        [System.IO.Path]::Combine($PSScriptRoot, 'SQL'),
-        [System.IO.Path]::Combine($PSScriptRoot, 'SystemRoles'),
-        [System.IO.Path]::Combine($PSScriptRoot, 'Tasks'),
-        [System.IO.Path]::Combine($PSScriptRoot, 'BBCRMCustomInstallManifest.txt')
-    )
+	# Keep track of a few extra things to clean up that might not be in the manifest
+	$cleanupArr = @(
+		[System.IO.Path]::Combine($PSScriptRoot, '..\..\vroot\bin\custom'),
+		[System.IO.Path]::Combine($PSScriptRoot, '..\..\vroot\browser\htmlforms\custom'),
+		[System.IO.Path]::Combine($PSScriptRoot, 'RevisionDlls'),
+		[System.IO.Path]::Combine($PSScriptRoot, 'SQL'),
+		[System.IO.Path]::Combine($PSScriptRoot, 'SystemRoles'),
+		[System.IO.Path]::Combine($PSScriptRoot, 'Tasks'),
+		[System.IO.Path]::Combine($PSScriptRoot, 'BBCRMCustomInstallManifest.txt')
+	)
 
-    foreach ($line in (Get-Content $ManifestFile)) {
-        if ($line.Length -gt 0) {
-            if (Test-Path $line) {
-                $item = Get-Item $line
-                if ($item -is [System.IO.DirectoryInfo]) {
-                    Log-Info "Encountered directory '$line', queueing for later cleanup"
-                    $cleanupArr += $item
-                } else {
-                    Log-Info "Removing '$line'"
-                    Remove-Item $line
-                }
-            } else {
-                $msg = "Did not find expected file: '$line'"
-                $warnings += $msg
-                Log-Warning $msg
-            }
-        }
-    }
+	foreach ($line in (Get-Content $ManifestFile)) {
+		if ($line.Length -gt 0) {
+			if (Test-Path $line) {
+				$item = Get-Item $line
+				if ($item -is [System.IO.DirectoryInfo]) {
+					Log-Info "Encountered directory '$line', queueing for later cleanup"
+					$cleanupArr += $item
+				} else {
+					Log-Info "Removing '$line'"
+					Remove-Item $line
+				}
+			} else {
+				$msg = "Did not find expected file: '$line'"
+				$warnings += $msg
+				Log-Warning $msg
+			}
+		}
+	}
 
-    foreach ($cleanup in $cleanupArr) {
-        if (Test-Path $cleanup) {
-            $cleanupItem = Get-Item $cleanup
-            if ($cleanupItem -is [System.IO.DirectoryInfo]) {
-                if (Check-DirectoryIsEmpty $cleanupItem) {
-                    Log-Info "Removing empty directory '$cleanupItem'"
-                    Remove-Item $cleanupItem -Force -Recurse
-                } else {
-                    Log-Info "Directory '$cleanupItem' is not empty, will not remove"
-                }
-            }
-        }
-    }
-    
+	foreach ($cleanup in $cleanupArr) {
+		if (Test-Path $cleanup) {
+			$cleanupItem = Get-Item $cleanup
+			if ($cleanupItem -is [System.IO.DirectoryInfo]) {
+				if (Check-DirectoryIsEmpty $cleanupItem) {
+					Log-Info "Removing empty directory '$cleanupItem'"
+					Remove-Item $cleanupItem -Force -Recurse
+				} else {
+					Log-Info "Directory '$cleanupItem' is not empty, will not remove"
+				}
+			}
+		}
+	}
+	
 } 
 catch 
 {
-    Log-Error $_.Exception
-    if (-not($NonInteractive)) {
-        Read-Host "Error encountered, see '$LogFile' for detail.  Press any key to continue..." > $null
-    } else {
-        throw
-    }
+	Log-Error $_.Exception
+	if (-not($NonInteractive)) {
+		Read-Host "Error encountered, see '$LogFile' for detail.  Press any key to continue..." > $null
+	} else {
+		throw
+	}
 }
 #finally
 #{
