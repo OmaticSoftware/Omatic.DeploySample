ef158edfb3c8d1426951e662453fa3ec7dcef770
Matt Hall <matt.hall@omaticsoftware.com>
2016-03-22 21:42:29 -0400

    Added UI model + HTML

---
+++ Omatic.DeploySample.UIModel/DeploySample/OmaticDeploySampleAddDataFormUIModel.vb
@@ -0,0 +1,7 @@
+Public Class OmaticDeploySampleAddDataFormUIModel
+
+    Private Sub OmaticDeploySampleAddDataFormUIModel_Loaded(ByVal sender As Object, ByVal e As Blackbaud.AppFx.UIModeling.Core.LoadedEventArgs) Handles Me.Loaded
+
+    End Sub
+
+End Class
